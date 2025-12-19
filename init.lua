d3c401ec0004c996d8caef29d49dafb4a13bb81a
devvime <dihapp@outlook.com>
2025-12-19 19:14:41 -0300

    fix: init.lua

--- init.lua
+++ init.lua
@@ -3,21 +3,21 @@
 -- ======================
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "https://github.com/folke/lazy.nvim",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"https://github.com/folke/lazy.nvim",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-  callback = function()
-    vim.lsp.buf.format()
-  end,
+	callback = function()
+		vim.lsp.buf.format()
+	end,
 })
 
 require("keymaps")
@@ -27,6 +27,12 @@ vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+
+-- NvimTree
+vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NvimTreeNormalNC", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NvimTreeEndOfBuffer", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NvimTreeVertSplit", { bg = "NONE" })
 
 -- ======================
 -- VS Code-like options
