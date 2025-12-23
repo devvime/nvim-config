b27b08a9ec6751b283c9b6f36bc7648675a885f5
devvime <dihapp@outlook.com>
2025-12-23 02:04:26 -0300

    fix: lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,21 +5,45 @@ return {
     "williamboman/mason-lspconfig.nvim",
   },
   config = function()
+    -- Mason
     require("mason").setup()
 
+    -- LSPs que serão instalados automaticamente
     require("mason-lspconfig").setup({
       ensure_installed = {
-        "ts_ls",     -- TypeScript / Angular
+        -- JS / TS
+        "ts_ls",
+
+        -- Web
         "html",
         "cssls",
+        "jsonls",
+
+        -- Backend
         "pyright",
+        "intelephense",
+
+        -- C / C++
+        "clangd",
+
+        -- C#
+        "omnisharp",
+
+        -- Docker
+        "dockerls",
       },
+      automatic_installation = true,
     })
 
-    -- ✅ NOVA API OFICIAL (Neovim 0.11+)
+    -- ⚠️ Neovim 0.11+ (nova API)
     vim.lsp.config("ts_ls", {})
     vim.lsp.config("html", {})
     vim.lsp.config("cssls", {})
+    vim.lsp.config("jsonls", {})
     vim.lsp.config("pyright", {})
+    vim.lsp.config("intelephense", {})
+    vim.lsp.config("clangd", {})
+    vim.lsp.config("omnisharp", {})
+    vim.lsp.config("dockerls", {})
   end,
 }
