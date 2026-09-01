7f666e71fb6e2bcab14a97987ffb8512da562c5e
devvime <dihapp@outlook.com>
2026-09-01 19:36:37 -0300

    feat: add treesitter

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -5,6 +5,9 @@ return {
     config = function()
       require("vscode").setup({
         transparent = true,
+        italic_comments = true,
+        underline_links = true,
+        disable_nvimtree_bg = true,
       })
       vim.cmd.colorscheme("vscode")
     end,
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,40 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "lua",
+        "vim",
+        "vimdoc",
+        "javascript",
+        "typescript",
+        "tsx",
+        "html",
+        "css",
+        "scss",
+        "json",
+        "python",
+        "php",
+        "c",
+        "cpp",
+        "c_sharp",
+        "java",
+        "dockerfile",
+        "bash",
+        "markdown",
+        "markdown_inline",
+        "yaml",
+      },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = {
+        enable = true,
+      },
+    })
+  end,
+}
